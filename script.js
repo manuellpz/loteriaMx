d6bc0af33157ca0a1ecc91815ff451e37f167fbe
ManuelLpz <manuel@mztweb.com>
2021-11-22 18:47:15 -0700

    Nueva funcionalidad

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const items_loteria = document.querySelectorAll('.items_loteria');
 let cartas_restantes = document.querySelector('#cartas_restantes');
+const btnGenerate = document.querySelector('#btnGenerate');
 let randomNumber = Math.floor((Math.random()*54)+1);
-let cards = [randomNumber];
+let cards = [17,32,8,31,46,16,6,9,52,18,19,15,7,44,43,27];
 
-//Generamos un arreglo con numeros random que representan a las 54 cartas
-for(let i=0;i<15;i++){
-    randomNumber = Math.floor((Math.random()*54)+1);
-    while(cards.includes(randomNumber)){
+const generateRandomCard = () => {
+    //Generamos un arreglo con numeros random que representan a las 54 cartas
+    cards = [randomNumber]
+
+    for(let i=0;i<15;i++){
         randomNumber = Math.floor((Math.random()*54)+1);
+        while(cards.includes(randomNumber)){
+            randomNumber = Math.floor((Math.random()*54)+1);
+        }
+        cards.push(randomNumber);
     }
-    cards.push(randomNumber);
+        items_loteria.forEach((item,index)=>{
+        let card_number = cards[index];
+        item.style.background = `url('img/Carta (${card_number}).jpg')`;
+        item.style.backgroundSize='cover';
+    })
 }
+
+// btnGenerate.addEventListener('click',generateRandomCard())
+
 
 //Generamos las cartas random de acuerdo con el arreglo que generamos con anterioridad
 items_loteria.forEach((item,index)=>{
